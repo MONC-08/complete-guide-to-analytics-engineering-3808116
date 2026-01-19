a3e12d35bf958e8fec40f73d639490fcacc1ae66
Chowdhury <88639391+MONC-08@users.noreply.github.com>
2026-01-19 21:46:11 +0000

    sql joins and CTE

--- sql_practice.sql
+++ sql_practice.sql
@@ -106,3 +106,98 @@ select strftime('%Y-%m-01', OrderDate) as FirstOfMonth
   , sum(OrderTotal) as OrderTotalSum
 from red_30_sales_cleaned
 group by FirstOfMonth
+
+-- inner join
+
+select * 
+from red_30_sales_cleaned as sc
+inner join red_30_tech_us_customer_info as ci
+  on sc.OrderNum = ci.OrderNum
+
+
+select ci.OrderNum
+  , ci.CustName
+  , ci.CustomerType
+  , ci.CustState
+  , sr.State
+  , sr.Region
+  , sc.state_name
+  , sc.state_code 
+  , sc.state_timezone
+from red_30_tech_us_customer_info as ci
+inner join red_30_tech_us_sales_regions as sr 
+  on ci.CustState = sr.State
+inner join us_state_codes as sc 
+  on ci.CustState = sc.state_name
+
+-- Left join
+
+select *
+from red_30_sales_cleaned as sc 
+left join red_30_tech_us_product_info as pi
+  on sc.OrderNum = pi.OrderNum
+
+
+select sr.State
+  , sr.Region
+  , usc.state_name
+  , usc.state_code
+from red_30_tech_us_sales_regions as sr 
+left join us_state_codes as usc 
+  on sr.State = usc.state_name
+
+
+select sr.State
+  , sr.Region
+  , usc.state_name
+  , usc.state_code
+from us_state_codes as usc
+left join red_30_tech_us_sales_regions as sr
+  on sr.State = usc.state_name
+
+
+select *
+from red_30_sales_cleaned as sc 
+left join red_30_tech_us_sales_associates as sa 
+  on sc.EmpID = sa.EmpID
+left join red_30_tech_us_customer_info as ci 
+  on sc.OrderNum = ci.OrderNum
+left join red_30_tech_us_sales_regions as sr 
+  on ci.CustState = sr.State
+
+
+  -- Common Table Expression (CTE)
+
+
+WITH dupe_identifier AS (
+    SELECT *,
+           row_number() OVER (
+               PARTITION BY OrderNum, OrderDate, EmpID
+               ORDER BY OrderNum
+           ) AS row_nbr
+    FROM red_30_tech_us_sales
+)
+SELECT *
+FROM dupe_identifier
+WHERE row_nbr = 1;
+
+select *
+from red_30_tech_us_sales
+where OrderNum in (1102922)
+
+-- ...................................
+
+with order_product_discounts as (
+select sc.EmpID
+  , pi.ProdCategory
+  , sum(Discount) as DiscountSum
+from red_30_sales_cleaned as sc 
+left join red_30_tech_us_product_info as pi 
+  on sc.OrderNum = pi.OrderNum
+group by sc.EmpID
+  , pi.ProdCategory
+)
+
+select *
+  , rank() over (partition by ProdCategory order by DiscountSum desc) as DiscountedRank
+from order_product_discounts
